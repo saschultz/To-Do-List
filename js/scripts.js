b8ccce170c169698135390e610ae163f0eb632aa
Niklas Long and Sara Schultz <sara@>
2017-04-03 10:29:53 -0700

    initialize project directories and files, add basic business and UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
-//Back-End Logic:
-function Contact(first, last) {
-  this.firstName = first;
-  this.lastName = last;
+// Business logic
+
+function Places(placeName, time, landmarks, notes) {
+  this.placeName = placeName;
+  this.time = time;
+  this.landmarks = landmarks;
+  this.notes = notes;
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+Places.prototype.nameYear = function() {
+  return this.placeName + " - " + this.time;
 }
 
-//Front-End Logic:
-$(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
 
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+// UI logic
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+$(function() {
+  $("form#places").submit(function(e) {
+    e.preventDefault();
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    var inputName = $("input#name").val();
+    var inputTime = $("input#time").val();
+    var inputLandmarks = $("input#landmarks").val();
+    var inputNotes = $("input#notes").val();
 
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-    });
+    var newPlaces = new Places(inputName, inputTime, inputLandmarks, inputNotes);
+
+    $("input#name, input#time, input#landmarks, input#notes").val("");
+
+    $("#name-output").append("<li>" + newPlaces.nameYear() + "</li>");
   });
+
 });
